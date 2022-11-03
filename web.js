d4823f90ddf33bf49a10abdec094fbd5e1f1d46d
Guy248 <109737364+Guy248@users.noreply.github.com>
2022-11-03 20:00:42 +0200

    Add files via upload

---
+++ web.js
@@ -0,0 +1,21 @@
+"use strict";
+function instructions(index) {
+  // Get the modal
+  let modal = document.getElementById("instructions" + index);
+  modal.style.display = "block";
+
+  // Get the <span> element that closes the modal
+  let span = document.getElementsByClassName("close")[index];
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function () {
+    modal.style.display = "none";
+  };
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+}
